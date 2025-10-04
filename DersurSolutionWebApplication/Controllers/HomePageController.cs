793c9a319ca69c46fafce20ef2fcf8033c7e1a75
lewis gecarane <lewisleander.gecarane.cics@ust.edu.ph>
2025-10-04 13:12:12 +0800

    Add project files.

---
+++ DersurSolutionWebApplication/Controllers/HomePageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DersurSolutionWebApplication.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult ServicePage()
+        {
+            return View();
+        }
+
+        public ActionResult PortfolioPage()
+        {
+            return View();
+        }
+        public ActionResult AboutPage()
+        {
+            return View();
+        }
+        public ActionResult ContactPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ DersurSolutionWebApplication/Scripts/HolderScripts/Controller.js
@@ -0,0 +1,22 @@
+﻿
+app.controller("DersurSolutionWebApplicationController", function ($scope) {
+    // Form submit function using ng-click
+    $scope.submitData = function () {
+        if (!$scope.formData.firstName || !$scope.formData.lastName || !$scope.formData.email || !$scope.formData.phone || !$scope.formData.message) {
+            alert("Please fill out all fields!");
+            return;
+        }
+
+        // Store data in multidimensional array
+        $scope.submittedData.push({
+            firstName: $scope.formData.firstName,
+            lastName: $scope.formData.lastName,
+            email: $scope.formData.email,
+            phone: $scope.formData.phone,
+            message: $scope.formData.message
+        });
+
+        // Clear form after submission
+        $scope.formData = {};
+    };
+});
